20a60096ad7f2fcc2cf213b5e9c68087d6782404
SFirigua <119648805+SFirigua@users.noreply.github.com>
2024-10-20 21:28:52 -0500

    Cambio en la base de datos

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -15,7 +15,9 @@ CREATE TABLE Transportes (
     id_transporte INT AUTO_INCREMENT PRIMARY KEY,
     tipo_transporte ENUM('Avión', 'Tren', 'Autobús') NOT NULL,
     nombre_transporte VARCHAR(100) NOT NULL,
-    num_asientos INT NOT NULL
+    num_asientos INT NOT NULL,
+    id_ruta INT,
+    FOREIGN KEY (id_ruta) REFERENCES Rutas(id_ruta)
 );
 
 CREATE TABLE Viajes (
@@ -28,26 +30,18 @@ CREATE TABLE Viajes (
     fecha_llegada DATE NOT NULL,
     hora_llegada TIME NOT NULL,
     precio DECIMAL(10, 2) NOT NULL,
- estado ENUM('Programado', 'Cancelado', 'En curso', 'Finalizado') DEFAULT 'Programado',
- FOREIGN KEY (id_transporte) REFERENCES Transportes(id_transporte)
+    estado ENUM('Programado', 'Cancelado', 'En curso', 'Finalizado') DEFAULT 'Programado',
+    FOREIGN KEY (id_transporte) REFERENCES Transportes(id_transporte)
 );
 
 CREATE TABLE Rutas (
     id_ruta INT AUTO_INCREMENT PRIMARY KEY,
-    id_transporte INT,
+    nombre_ruta VARCHAR(255) NOT NULL,
     origen VARCHAR(100) NOT NULL,
     destino VARCHAR(100) NOT NULL,
     duracion TIME NOT NULL,
     paradas_intermedias VARCHAR(255),
     frecuencia ENUM('Diaria', 'Semanal', 'Mensual') DEFAULT 'Diaria',
-    FOREIGN KEY (id_transporte) REFERENCES Transportes(id_transporte)
-);
-
-CREATE TABLE Paradas (
-    id_parada INT AUTO_INCREMENT PRIMARY KEY,
-    id_ruta INT,
-    nombre_parada VARCHAR(100) NOT NULL,
-    FOREIGN KEY (id_ruta) REFERENCES Rutas(id_ruta)
 );
 
 CREATE TABLE Reservas (
@@ -56,17 +50,8 @@ CREATE TABLE Reservas (
     id_viaje INT,
     fecha_reserva DATE NOT NULL,
     reservas_vendidas INT NOT NULL,
+    asiento ENUM('Economica', 'Premium', 'Ejecutiva') NOT NULL,
     estado ENUM('Pendiente', 'Confirmada', 'Cancelada') DEFAULT 'Pendiente',
     FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
     FOREIGN KEY (id_viaje) REFERENCES Viajes(id_viaje)
-);
-
-CREATE TABLE Asientos (
-    id_asiento INT AUTO_INCREMENT PRIMARY KEY,
-    id_viaje INT,
-    id_reserva INT,
-    numero_asiento VARCHAR(10) NOT NULL UNIQUE,
-    clase ENUM('Economica', 'Premium', 'Ejecutiva') NOT NULL,
-    FOREIGN KEY (id_viaje) REFERENCES Viajes(id_viaje),
-    FOREIGN KEY (id_reserva) REFERENCES Reservas(id_reserva)
 );
